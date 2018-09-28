e4234a2c35852c5d41f0cc907d08aa6a040f1661
natekarnes <37960777+natekarnes@users.noreply.github.com>
2018-09-28 16:11:21 -0700

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { Data } from '../providers/data/data';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  providers: [StatusBar, SplashScreen, Data, {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ]
+})
+export class AppModule {}
